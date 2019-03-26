2631a6be392b922195fbdef6c4781c1a170c43cd
sean miller <>
2019-03-26 15:23:33 -0700

    Fix merge sort

--- Sort/Merge/merge_sort.js
+++ Sort/Merge/merge_sort.js
@@ -1,11 +1,8 @@
 const merge = (arr1, arr2) => {
-
   let arr = [];
-
   let i = 0;
   let j = 0;
 
-  
   while(i < arr1.length && j < arr2.length) {
 
     if(arr2[j] > arr1[i]) {
@@ -34,27 +31,11 @@ const merge = (arr1, arr2) => {
 
 const mergeSort = (arr) => {
 
-  
-  const split = (arr) => {
-
-    let splitArray = [];
-
-    
-    let midpoint = Math.floor(arr.length / 2);
-    let arr1 = arr.slice(0, midpoint);
-    let arr2 = arr.slice(midpoint);
-    console.log(arr1)
-    console.log(arr2);
-
-    
-
-  }
-
-  const tester = split(arr);
-
-
-
-  console.log("tester is:", tester)
+  if(arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
 
 }
 
